3bc9d85ec172de6656b881e8c733ee2ae0270276
pochi01 <clear_sky_autumn10@yahoo.co.jp>
2019-01-04 23:29:53 +0900

    Add Report show

--- src/controllers/reports/ReportsShowServlet.java
+++ src/controllers/reports/ReportsShowServlet.java
@@ -2,23 +2,28 @@ package controllers.reports;
 
 import java.io.IOException;
 
+import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import models.Report;
+import utils.DBUtil;
+
 /**
  * Servlet implementation class ReportShowServlet
  */
-@WebServlet(name = "ReportsShowServlet", urlPatterns = { "/reports/show" })
-public class ReportShowServlet extends HttpServlet {
+@WebServlet("/reports/show" )
+public class ReportsShowServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public ReportShowServlet() {
+    public ReportsShowServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -27,8 +32,18 @@ public class ReportShowServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		EntityManager em = DBUtil.createEntityManager();
+
+		Report r = em.find(Report.class,Integer.parseInt(request.getParameter("id")));
+
+		em.close();
+
+		request.setAttribute("report", r);
+		request.setAttribute("_token", request.getSession().getId());
+
+		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/reports/show.jsp");
+		rd.forward(request, response);
+
 	}
 
 }
